9ab4280f929332af8af8df3b6944fd6c60dd9f04
Lior Sukman <lior.sukman@gmail.com>
2020-12-25 20:23:34 +0200

    model comparison nb

--- NN_pert_gen.py
+++ NN_pert_gen.py
@@ -111,7 +111,7 @@ def test(model, device, test_loader, cnn):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Perturbation Network Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
